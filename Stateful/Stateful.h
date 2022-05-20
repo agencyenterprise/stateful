5edcb7c8c5fe38b41fc460d642ba2a8ccbb169a3
Lucas Assis Rodrigues <lucas.assis.ro@gmail.com>
2022-05-19 19:57:39 -0700

    Initial Commit

---
+++ Stateful/Stateful.h
@@ -0,0 +1,18 @@
+//
+//  Stateful.h
+//  Stateful
+//
+//  Created by Lucas Assis Rodrigues on 5/19/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Stateful.
+FOUNDATION_EXPORT double StatefulVersionNumber;
+
+//! Project version string for Stateful.
+FOUNDATION_EXPORT const unsigned char StatefulVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Stateful/PublicHeader.h>
+
+
